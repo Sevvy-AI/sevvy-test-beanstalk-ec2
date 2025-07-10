322dc307be59a5ba18e2b6cedd61fffeef5b1b8d
Mitch Radhuber <radhuber@umich.edu>
2025-07-09 20:52:38 -0400

    adding sentry to flask server

--- application.py
+++ application.py
@@ -4,6 +4,23 @@ import traceback
 from datetime import datetime
 from flask import Flask, request, jsonify
 import os
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://e22cad5c2631689d5edfac56e92ee4c0@o4509641426796544.ingest.us.sentry.io/4509641441673216",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profile_session_sample_rate to 1.0 to profile 100%
+    # of profile sessions.
+    profile_session_sample_rate=1.0,
+    # Set profile_lifecycle to "trace" to automatically
+    # run the profiler on when there is an active transaction
+    profile_lifecycle="trace",
+)
 
 # Configure logging for CloudWatch
 logging.basicConfig(
